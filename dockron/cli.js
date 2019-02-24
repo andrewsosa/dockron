b1a7ec20268282edddd61b8bc18730738b9e1af2
Andrew Sosa <andrew@andrewsosa.com>
2019-02-24 11:59:50 -0500

    Add joi for config schema validation

--- dockron/cli.js
+++ dockron/cli.js
@@ -2,6 +2,6 @@ const path = require('path');
 const config = require('./config');
 const scheduler = require('./scheduler');
 
-config(path.join(process.cwd(), 'tests/configs/sample.toml'), conf => {
-    Object.keys(conf).map(name => scheduler.schedule(name, conf[name]));
-});
+config(path.join(process.cwd(), 'example/sample.toml'), conf =>
+    conf.map(spec => scheduler.schedule(spec.name, spec)),
+);
--- dockron/config.js
+++ dockron/config.js
@@ -1,11 +1,40 @@
 const fs = require('fs');
+const joi = require('joi');
 const toml = require('toml');
+const logger = require('./logger')(__filename);
+
+const tagRegex = /(([a-zA-Z0-9]+)(:[0-9]+)?\/)?([a-zA-Z0-9]+\/)?([a-zA-Z0-9]+)(:[a-zA-Z0-9]+)?/;
+
+const specSchema = joi.object().keys({
+    name: joi.string().required(),
+    schedule: joi.string().required(),
+    image: joi
+        .string()
+        .regex(tagRegex)
+        .required(),
+    command: joi.string().required(),
+});
+
+const specArray = joi.array().items(specSchema);
+
+const flatten = conf =>
+    Object.keys(conf).map(name => Object.assign(conf[name], { name }));
 
 module.exports = (path, fn) => {
     const confpath = path || '/etc/dockron/dockron.toml';
     fs.readFile(confpath, (err, data) => {
-        if (err) throw err;
+        if (err) {
+            logger.error(err);
+            return;
+        }
         const parsed = toml.parse(data);
-        fn(parsed);
+        const flat = flatten(parsed);
+        joi.validate(flat, specArray, valErr => {
+            if (valErr) {
+                logger.error(valErr);
+                return;
+            }
+            fn(flat);
+        });
     });
 };
