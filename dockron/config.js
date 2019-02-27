41087f19b2c078bad720ee195ebfe40aacec8ca7
Andrew Sosa <andrew@andrewsosa.com>
2019-02-26 22:32:32 -0500

    Add environment variable interpolation for config

--- dockron/config.js
+++ dockron/config.js
@@ -18,13 +18,28 @@ const specSchema = joi.object().keys({
 
 const specArray = joi.array().items(specSchema);
 
+const envsub = stringContent => {
+    const regex = /\$(?:(\w+)|{(\w+)})/g;
+    regex.exec(stringContent);
+
+    return stringContent.replace(regex, (original, g1, g2) => {
+        const variable = g1 || g2;
+
+        // eslint-disable-next-line no-prototype-builtins
+        return process.env.hasOwnProperty(variable)
+            ? process.env[variable]
+            : original;
+    });
+};
+
 const flatten = conf =>
     Object.keys(conf).map(name => Object.assign(conf[name], { name }));
 
 module.exports = (path, fn) => {
     try {
-        const data = fs.readFileSync(path);
-        const parsed = toml.parse(data);
+        const conf = fs.readFileSync(path, 'utf8');
+        const subbed = envsub(conf);
+        const parsed = toml.parse(subbed);
         const flat = flatten(parsed);
         joi.validate(flat, specArray);
         logger.debug('Loaded config at %s', path);
--- dockron/config.test.js
+++ dockron/config.test.js
@@ -5,6 +5,7 @@ const loadConfig = require('./config');
 
 const validPath = path.join(__dirname, '../example/sample.toml');
 const malformedPath = path.join(__dirname, '../example/invalid.toml');
+const envvarPath = path.join(__dirname, '../example/envvar.toml');
 
 describe('the config parser', () => {
     test('loads a valid config', () =>
@@ -20,5 +21,12 @@ describe('the config parser', () => {
 
     test('rejects malforned config', () => {
         expect(() => loadConfig(malformedPath)).toThrow(/Expected/);
+    });
+
+    test('interpolates environment variables', () => {
+        process.env.TEST_VAR = 'testvar';
+        loadConfig(envvarPath, conf => {
+            expect(JSON.stringify(conf)).toMatch(/testvar/);
+        });
     });
 });
